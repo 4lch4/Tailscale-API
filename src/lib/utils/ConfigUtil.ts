7a8168136e42911f8411708394780ead9ca1c0de
4lch4 <git@4lch4.email>
2022-06-23 08:31:55 -0500

    fix: removed debug logging from ConfigUtil#loadConfig

--- src/lib/utils/ConfigUtil.ts
+++ src/lib/utils/ConfigUtil.ts
@@ -8,7 +8,7 @@
  * —————————————————————————————————————————————————————————————————————————————
  * File Path				⸺ /src/lib/utils/ConfigUtil.ts
  * File Created			⸺ 2022-06-22 @ 17:52:33-05:00
- * Last Modified		⸺ 2022-06-23 @ 00:53:30-05:00
+ * Last Modified		⸺ 2022-06-23 @ 08:31:30-05:00
  * Modified By			⸺ Devin W. Leaman (4lch4) (hey@4lch4.email)
  * —————————————————————————————————————————————————————————————————————————————
  * MIT License			⸺ https://opensource.org/licenses/MIT
@@ -17,7 +17,6 @@
  * —————————————————————————————————————————————————————————————————————————————
  */
 
-import { logger } from '@4lch4/logger'
 import { config } from 'dotenv'
 import fs from 'fs-extra'
 import { dirname, join } from 'path'
@@ -45,15 +44,7 @@ export class ConfigUtil {
    * @returns The contents of the `.env` file or `undefined` if the file does not exist.
    */
   async loadConfig() {
-    if (await this.dotEnvExists()) {
-      logger.info(`dotEnvExists = true`)
-      const dotEnvRes = config({ path: this.dotEnvPath })
-
-      return dotEnvRes
-    } else {
-      logger.info(`dotEnvExists = false`)
-
-      return undefined
-    }
+    if (await this.dotEnvExists()) return config({ path: this.dotEnvPath })
+    else return undefined
   }
 }
